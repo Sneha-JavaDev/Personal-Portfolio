b33b30e4506570bcc0ac1084c361a9e96ec171f4
Sneha-JavaDev <aec.cse.snehamondal@gmail.com>
2025-07-17 10:29:48 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Full Stack Developer", "Java Developer", "Programmer","Learner"],
-    typeSpeed : 50,
-    backSpeed : 50,
-    backDelay : 1000,
-    loop : true
-});
